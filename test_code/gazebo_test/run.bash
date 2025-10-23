6153163bf9ba82a83a1e087da92c781dbe46c430
Jeremy Leicht <jeremyleicht@outlook.com>
2025-10-23 15:44:24 -0500

    added IMU but it didn't fix anything

--- test_code/gazebo_test/run.bash
+++ test_code/gazebo_test/run.bash
@@ -1,6 +1,9 @@
 source /home/jeremy9/maze/gazebo_test/install/setup.bash
 source /opt/ros/humble/setup.bash
+ros2 run ros_gz_bridge parameter_bridge /lidar2@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan --ros-args -r /lidar2:=/laser_scan
+ros2 run ros_gz_bridge parameter_bridge /model/vehicle_blue/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V
 ros2 run ros_gz_bridge parameter_bridge /model/vehicle_blue/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry &
 ros2 run ros_gz_bridge parameter_bridge /model/vehicle_blue/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist &
+ros2 run ros_gz_bridge parameter_bridge /imu@sensor_msgs/msg/Imu@ignition.msgs.IMU &
 ros2 run movement move_square &
 ign gazebo -v4 --render-engine ogre -r visualize_lidar.sdf
--- test_code/gazebo_test/src/movement/movement/move_square.py
+++ test_code/gazebo_test/src/movement/movement/move_square.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
 import math
 import time
 from tf_transformations import euler_from_quaternion
@@ -10,10 +11,13 @@ class MoveSquare(Node):
     def __init__(self):
         super().__init__('move_square')
         # Publisher for /cmd_vel
-        self.publisher_ = self.create_publisher(Twist, '/model/vehicle_blue/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/model/vehicle_blue/cmd_vel', 1)
         # Subscriber for /model/vehicle_blue/odometry
         self.odom_sub_ = self.create_subscription(
             Odometry, '/model/vehicle_blue/odometry', self.odom_callback, 10)
+        self.imu_sub_ = self.create_subscription(
+            Imu, '/imu', self.imu_callback, 10
+        )
         self.timer = self.create_timer(0.05, self.timer_callback)  # 20 Hz
         self.step = 0
         self.start_position = {'x': 0.0, 'y': 0.0, 'theta': 0.0}
@@ -26,6 +30,27 @@ class MoveSquare(Node):
         self.turn_start_time = None
         self.stop_time = 0
         self.target_angle = 90
+        self.last_imu_update = 0
+        self.rotational_velocity = 0
+        self.current_time = 0
+        self.rotation_start_time = 0
+        
+
+    def imu_callback(self, msg):
+        sim_time = msg.header.stamp.sec + msg.header.stamp.nanosec / 10E9
+        if self.last_imu_update == 0:
+            self.last_imu_update = sim_time
+            self.rotational_velocity = msg.angular_velocity.z
+            return
+        
+        avg_rv = (msg.angular_velocity.z + self.rotational_velocity) / 2
+        self.rotational_velocity = msg.angular_velocity.z
+
+        theta_diff = avg_rv * (sim_time - self.last_imu_update) * 180 / math.pi
+        self.last_imu_update = sim_time
+
+        #self.position['theta'] += theta_diff
+        
 
     def odom_callback(self, msg):
         # Update position from odometry
@@ -37,15 +62,27 @@ class MoveSquare(Node):
 
         _, _, self.position['theta'] = euler_from_quaternion(quat)
         self.position['theta'] = math.degrees(self.position['theta'])
+
+        self.current_time = msg.header.stamp.sec + msg.header.stamp.nanosec / 10E9
         #print(self.position)
 
     def pause(self):
         return (time.time() - self.stop_time > 5, (0.0, 0.0))
 
+    def angular_diff(self, θ1, θ2):
+        no_circle = abs(θ1 - θ2)
+        circle = abs((360 - max(θ1, θ2)) + min(θ1, θ2))
+
+        if no_circle < circle:
+            return no_circle
+        else: return -circle
+
     def rotate(self):
-        theta_diff = abs(self.position['theta']) - abs(self.target_angle)
-        print(theta_diff, self.position['theta'], self.target_angle)
-        return (abs(theta_diff) <= 0.5, (0.0, (-1 if theta_diff < 0 else 1) * 0.5))
+        if self.rotation_start_time == 0:
+            self.rotation_start_time = self.current_time
+        theta_diff = self.angular_diff(self.position['theta'] + 180, self.target_angle)
+        #print(theta_diff, self.position['theta'] + 180, self.target_angle)
+        return (self.current_time - self.rotation_start_time >= 3.14, (0.0, -0.5 if abs(theta_diff) > 20 else -0.1))
 
     def straight(self):
         dist = math.sqrt((self.position['x'] - self.start_position['x']) ** 2 + (self.position['y'] - self.start_position['y']) ** 2)
@@ -55,6 +92,7 @@ class MoveSquare(Node):
     def timer_callback(self):
         msg = Twist()
         # msg.linear.x, msg.angular.z
+
 
         if self.step == 0:
             result = self.straight()
@@ -74,7 +112,7 @@ class MoveSquare(Node):
                 self.target_angle = (self.target_angle + 90) % 360
             self.start_position = {'x': self.position['x'], 'y': self.position['y']}
             if self.step > 3: self.step = 0
-        #print(f"X: {self.position['x']: .1f}, Y: {self.position['y']: .1f}, θ: {self.position['theta']}")
+        print(f"X: {self.position['x']: .1f}, Y: {self.position['y']: .1f}, θ: {self.position['theta']}")
         #print(self.step)
         
         msg.linear.x = result[1][0]
