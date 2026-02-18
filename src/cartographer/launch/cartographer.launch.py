3f004dc4e4f6094bc5a3336f2434d56d70c25676
Jeremy Leicht <jeremyleicht@outlook.com>
2026-02-18 09:29:33 -0600

    added config and launch

---
+++ src/cartographer/launch/cartographer.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    config_dir = '/mnt/windows/Users/Jerem/Ubuntu/Code/robotics-research/MAZE_ROS/test_code/gazebo_test/src/my_cartographer/config'
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+
+    return LaunchDescription([
+
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=[
+                '-configuration_directory', config_dir,
+                '-configuration_basename', 'gazebo_2d.lua'
+            ],
+        ),
+    ])
+
+
