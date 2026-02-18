66d86fea6f65f8d780cd39c8b8f695866780df0d
Jeremy Leicht <jeremyleicht@outlook.com>
2026-02-18 10:53:55 -0600

    moved research stuff to research branch

--- test_code/gazebo_test/test.py
+++
@@ -1,8 +0,0 @@
-def angular_diff(θ1, θ2):
-    no_circle = abs(θ1 - θ2)
-    circle = abs((360 - max(θ1, θ2)) + min(θ1, θ2))
-
-    if no_circle < circle:
-        return no_circle
-    else: return -circle
-
